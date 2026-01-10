2b7c9832c5347fcbb7bef5d49893d5e98e89b4de
dohyung <154952683+dohyung001@users.noreply.github.com>
2026-01-11 02:54:04 +0900

    [Fix] : zustand 예시 수정

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -3,12 +3,15 @@ import { create } from 'zustand';
 interface AuthState {
   name: string | null;
   token: string | null;
+  setAuth: (name: string, token: string) => void;
+  resetAuth: () => void;
 }
+
 const useAuthStore = create<AuthState>((set) => ({
-  name: '',
+  name: null,
   token: null,
-  set: (name: string, token: string) => set({ name, token }),
-  reset: () => set({ name: '', token: null }),
+  setAuth: (name, token) => set({ name, token }),
+  resetAuth: () => set({ name: null, token: null }),
 }));
 
 export default useAuthStore;
