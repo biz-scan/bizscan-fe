22d37ecc99b4485af6ea8b260672c8a6f9775286
dohyung <154952683+dohyung001@users.noreply.github.com>
2026-01-25 18:41:34 +0900

    [Feat]: 버튼 공용 컴포넌트

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,27 +5,26 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 aria-invalid:border-destructive',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-lg typo-p2-semibold transition-colors disabled:pointer-events-none [&_svg]:pointer-events-none shrink-0 [&_svg]:shrink-0 outline-none cursor-pointer',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20',
-        outline: 'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground',
-        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
+        // 파란 버튼
+        default:
+          'bg-blue-normal text-white hover:bg-blue-normal-hover active:bg-blue-normal-active disabled:bg-blue-light-hover disabled:text-blue-light-active',
+        // 흰색 버튼
+        white: 'bg-grey-light text-grey-darker hover:text-grey-dark active:text-grey-normal-active',
+        // 테두리만 버튼
+        outline:
+          'border border-grey-normal text-grey-normal active:text-blue-light-active active:border-blue-light-active',
+        // 텍스트버튼
+        blueLight:
+          'bg-blue-light-active text-grey-ligth hover:bg-blue-light-hover active:bg-blue-light',
       },
       size: {
-        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
-        xs: 'h-6 gap-1 rounded-md px-2 text-xs has-[>svg]:px-1.5',
-        sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
-        lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
-        icon: 'size-9',
-        'icon-xs': 'size-6 rounded-md',
-        'icon-sm': 'size-8',
-        'icon-lg': 'size-10',
+        default: 'h-12 px-4.5 py-4.5',
+        lg: 'h-16 px-8 py-4.5 typo-p1-bold',
+        sm: 'h-10.5 px-5 py-2.5 typo-p2-semibold',
       },
     },
     defaultVariants: {
@@ -39,6 +38,7 @@ function Button({
   className,
   variant = 'default',
   size = 'default',
+
   asChild = false,
   ...props
 }: React.ComponentProps<'button'> &
