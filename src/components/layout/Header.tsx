bf05f47d824f4c743500f7608f766785de5cf54d
dohyung <carl2001@naver.com>
2026-01-27 20:01:43 +0900

    [Feat] : 헤더 구현

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,44 @@
-interface HeaderProps {
-  title?: string;
-}
+import { Link, useLocation } from 'react-router-dom';
 
-export default function Header({ title }: HeaderProps) {
+import Logo from '@/assets/icons/Logo/Logo.svg?react';
+import { Button } from '@/components/ui/Button';
+import useAuthStore from '@/store/useAuthStore';
+
+import HeaderConfirmDialog from './HeaderConfirmDialog';
+
+export default function Header() {
+  const location = useLocation();
+  const { isAuthenticated, user } = useAuthStore();
+
+  if (location.pathname === '/login' || location.pathname === '/register') {
+    return null;
+  }
+
   return (
-    <header className="bg-white border-b px-6 py-4">
-      {title ? (
-        <h2 className="text-lg font-semibold text-gray-800">{title}</h2>
+    <header className="bg-grey-light border-b h-20 flex items-center px-12 justify-between">
+      {location.pathname === '/' ? (
+        <Link to="/">
+          <Logo />
+        </Link>
+      ) : location.pathname === '/onboarding' ? (
+        <HeaderConfirmDialog>
+          <Logo className="cursor-pointer" />
+        </HeaderConfirmDialog>
       ) : (
-        <h1 className="text-xl text-black font-bold">Bizscan</h1>
+        <Link to="/dashboard">
+          <Logo />
+        </Link>
+      )}
+
+      {isAuthenticated ? (
+        <div>
+          <p className="typo-p2-semibold text-grey-dark">{user?.name}님 환영합니다!</p>
+        </div>
+      ) : (
+        <div className="flex gap-3">
+          <Button variant="white">회원가입</Button>
+          <Button variant="default">로그인</Button>
+        </div>
       )}
     </header>
   );
