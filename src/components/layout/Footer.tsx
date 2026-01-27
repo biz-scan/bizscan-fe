0596ad715f0c3c2299814771c2d9f0665f30f316
sumin <sumin990103@naver.com>
2026-01-28 03:08:37 +0900

    feat: footer ui

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,35 @@
+import BizScanLogo from '@/assets/icons/Logo/Logo.svg';
+
 export default function Footer() {
   return (
-    <footer className="border-t px-6 py-4 text-center text-sm text-gray-500">
-      2025 Bizscan
+    <footer className="w-full bg-grey-light-hover">
+      <div className="mx-auto w-full max-w-[1348px] py-10 text-p2 text-grey-dark">
+
+        <div className="grid grid-cols-2 gap-y-6">
+          <div className="flex flex-col gap-4 typo-p2-medium">
+            <img src={BizScanLogo} alt="BizScan" className="h-10 w-auto -ml-63" />
+            <p>데이터에서 실행으로, 내 가게 성공 레시피</p>
+          </div>
+
+          <div className="flex justify-end typo-p2-medium">
+            <p>Contact. bizscan@gmail.com</p>
+          </div>
+
+          <div className="col-span-2 flex items-center justify-between w-full">
+            <p className="typo-p2-semibold">
+              Copyright ⓒ BizScan. All rights reserved
+            </p>
+
+            <div className="flex items-center gap-4 typo-p2-medium">
+              <span>이용안내</span>
+              <span>|</span>
+              <span className="typo-p2-semibold">개인정보처리방침</span>
+              <span>|</span>
+              <span>운영정책</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
