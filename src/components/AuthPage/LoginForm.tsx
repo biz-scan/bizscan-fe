4317f5b5f80a37d120e5042b38a5d2f638f5d880
dohyung <carl2001@naver.com>
2026-01-30 02:56:20 +0900

    [Feat] : zod 스키마 생성 + 로그인 적용

--- src/components/AuthPage/LoginForm.tsx
+++ src/components/AuthPage/LoginForm.tsx
@@ -1,27 +1,30 @@
-import { type SubmitHandler, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 import { Checkbox } from '@/components/ui/Checkbox';
 import { Label } from '@/components/ui/Label';
+import { type LoginInput, loginSchema } from '@/schemas/auth.schema';
 
 import { Button } from '../ui/Button';
 import { Input } from '../ui/Input';
 
-type Inputs = {
-  email: string;
-  password: string;
-};
-
 interface LoginFormProps {
   handleTabChange?: () => void;
 }
+
 export default function LoginForm({ handleTabChange }: LoginFormProps) {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  } = useForm<LoginInput>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = (_data: LoginInput) => {
+    // TODO: 로그인 API 호출
+  };
+
   return (
     <div className="w-full flex flex-col items-center">
       <h2 className="text-grey-darker mb-16">로그인</h2>
@@ -31,28 +34,33 @@ export default function LoginForm({ handleTabChange }: LoginFormProps) {
           이메일
         </Label>
         <Input
-          className="mb-4"
           id="email"
           placeholder="이메일을 입력하세요."
-          {...register('email', { required: true })}
+          aria-invalid={!!errors.email}
+          {...register('email')}
         />
+        {errors.email && (
+          <span className="text-error typo-p2-medium mt-2">{errors.email.message}</span>
+        )}
 
-        <Label className="mb-2 " htmlFor="password">
+        <Label className="mb-2 mt-4" htmlFor="password">
           비밀번호
         </Label>
         <Input
-          className="mb-6"
           id="password"
           type="password"
           placeholder="비밀번호를 입력하세요."
-          {...register('password', { required: true })}
+          aria-invalid={!!errors.password}
+          {...register('password')}
         />
+        {errors.password && (
+          <span className="text-error typo-p2-medium mt-2">{errors.password.message}</span>
+        )}
 
-        <div className="mb-8 flex items-center gap-2">
+        <div className="mt-6 mb-8 flex items-center gap-2">
           <Checkbox variant="white" size="sm" id="remember-me" />
           <Label htmlFor="remember-me">로그인 상태 유지</Label>
         </div>
-        {errors.password && <span>This field is required</span>}
 
         <Button variant="default" type="submit" className="w-full">
           로그인
