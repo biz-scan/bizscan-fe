ceb1bceae6f886eed7223732549fad2c975ab9aa
prkhaeun <hani00415@naver.com>
2026-01-31 03:20:12 +0900

    [Fix]: 대시보드 및 레이아웃 UI 충돌 복구

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,22 +1,8 @@
 import { Outlet } from 'react-router-dom';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import Sidebar from '@/components/layout/Sidebar';
->>>>>>> eaa9d8b ([Feat]: 사이드바 공용 컴포넌트)
-=======
->>>>>>> 4b77e95 ([Feat]: Sidebar 아이콘 경로 수정 및 SVGR 컴포넌트 방식 적용)
 
 export default function MainLayout() {
   return (
     <div className="flex min-h-screen w-full">
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-      <Sidebar />
->>>>>>> eaa9d8b ([Feat]: 사이드바 공용 컴포넌트)
-=======
->>>>>>> 4b77e95 ([Feat]: Sidebar 아이콘 경로 수정 및 SVGR 컴포넌트 방식 적용)
       <div className="flex flex-1 flex-col min-w-0">
         <main className="flex-1 overflow-auto p-[50px] bg-[#F8FAFF]">
           <Outlet />
@@ -24,8 +10,4 @@ export default function MainLayout() {
       </div>
     </div>
   );
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> eaa9d8b ([Feat]: 사이드바 공용 컴포넌트)
