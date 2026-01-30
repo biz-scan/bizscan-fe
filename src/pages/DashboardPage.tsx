6faf0245090698e8d391c2b21e2b0ddfdf8d116f
prkhaeun <hani00415@naver.com>
2026-01-31 00:23:23 +0900

    [Feat]: 대시보드 히어로 섹션 구현 및 반응형 레이아웃 최적화

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,6 +1,7 @@
 export default function DashboardPage() {
   return (
 <<<<<<< HEAD
+<<<<<<< HEAD
     <div className="flex items-center justify-center w-full min-h-screen bg-white">
       <h1 className="text-[24px] font-bold text-gray-800">사이드바 테스트</h1>
 =======
@@ -9,15 +10,23 @@ export default function DashboardPage() {
       <h1 className="text-2xl font-bold mb-4">대시보드</h1>
       <p className="text-gray-500">대시보드 내용이 들어갈 영역입니다.</p>
 >>>>>>> eaa9d8b ([Feat]: 사이드바 공용 컴포넌트)
-    </div>
-  );
-}
 =======
-    <div className="flex items-center justify-center w-full min-h-screen bg-white">
-      <h1 className="text-[24px] font-bold text-gray-800">
-        사이드바 테스트
-      </h1>
+    <div
+      className="w-full bg-no-repeat bg-[center_20%] aspect-[1588/400] bg-cover max-h-[400px] min-h-[250px] flex items-center justify-center"
+      style={{ backgroundImage: "url('/images/background.png')" }}
+    >
+      <div className="flex flex-col items-center">
+        <h1 className="text-center text-Grey-Darker font-pretendard text-[clamp(28px,4vw,48px)] font-extrabold leading-[140%] tracking-[-1.2px]">
+          반갑습니다, 0000 님!
+        </h1>
+
+        <div className="mt-[28px] flex px-[24px] py-[12px] justify-center items-center rounded-[8px] bg-gra2 shadow-md">
+          <span className="text-blue-light font-pretendard text-[clamp(14px,1.2vw,16px)] font-normal leading-[140%] tracking-[-0.4px] whitespace-nowrap">
+            성수동 직장인 회식 1타
+          </span>
+        </div>
+      </div>
+>>>>>>> 15c31cb ([Feat]: 대시보드 히어로 섹션 구현 및 반응형 레이아웃 최적화)
     </div>
   );
 }
->>>>>>> 68dca4a ([Feat]: 사이드바 공용 컴포넌트)
