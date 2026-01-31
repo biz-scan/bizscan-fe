90511dc8b6eb0f4c879618b765a3e5f3b533fbe1
dohyung <154952683+dohyung001@users.noreply.github.com>
2026-02-01 01:45:29 +0900

    [Feat] : 랜딩페이지 UI

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,87 +1,54 @@
-import IconArrowRight from '@/assets/icons/Arrow/white.svg?react';
+import { useNavigate } from 'react-router-dom';
+
+import SearchIcon from '@/assets/icons/Icon/type=0.svg?react';
+import GraphIcon from '@/assets/icons/Icon/type=1.svg?react';
+import MarketingIcon from '@/assets/icons/Icon/type=2.svg?react';
+import ConsultingIcon from '@/assets/icons/Icon/type=3.svg?react';
+import MainIcon from '@/assets/icons/Icon/type=main.svg?react';
+import DescriptionBox from '@/components/HomePage/DescriptionBox';
 import { Button } from '@/components/ui/Button';
-import { Checkbox } from '@/components/ui/Checkbox';
-import { ChipFilter } from '@/components/ui/ChipFilter';
-import { Input } from '@/components/ui/Input';
-import { Label } from '@/components/ui/Label';
-import { RadioGroup, RadioGroupItem } from '@/components/ui/RadioGroup';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/Select';
-import { ToggleGroup, ToggleGroupItem } from '@/components/ui/ToggleGroup';
 
 export default function HomePage() {
+  const navigate = useNavigate();
   return (
-    <div className="flex flex-col items-center gap-10 p-10">
-      <div className="flex flex-col items-center gap-4">
-        {/* shadcn/ui 테스트 */}
-        <div className="pt-4 border-t border-grey-light">
-          {/* 타이포 테스트 */}
-          <h3 className="text-grey-normal text-p2-semibold mb-4">shadcn/ui Test</h3>
-          {/* 버튼 */}
-          <div className="flex gap-2">
-            <Button size="lg" className="w-[200px] justify-between">
-              디폴트
-              <IconArrowRight />
-            </Button>
-            <Button variant="white">white</Button>
-            <Button variant="outline">Outline</Button>
-            <Button variant="blueLight">blueLight</Button>
-          </div>
-          {/* 인풋 & 라벨 */}
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="email">Email</Label>
-            <Input type="email" id="email" placeholder="Email" />
-            <Input type="password" id="email" placeholder="비밀번호" />
-          </div>
-          {/* 체크박스 */}
-          <div className="flex items-center space-x-2">
-            <Checkbox variant="default" size="sm" />
-            <Checkbox variant="default" size="default" />
-          </div>
-          {/* 라디오 그룹 */}
-          <RadioGroup defaultValue="option-one">
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="option-one" id="option-one" />
-              <Label htmlFor="option-one">Option One</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="option-two" id="option-two" />
-              <Label htmlFor="option-two">Option Two</Label>
-            </div>
-          </RadioGroup>
-          {/* 칩 필터 */}
-          <div className="flex gap-2">
-            <ChipFilter active>Active Chip</ChipFilter>
-            <ChipFilter>Default Chip</ChipFilter>
-            <ChipFilter>Filter 1</ChipFilter>
-          </div>
+    <div className="flex flex-col w-full h-full justify-center items-center ">
+      <section
+        className="pt-35 flex flex-col items-center w-full min-w-fit bg-cover bg-top h-[500px]"
+        style={{ backgroundImage: "url('/images/background.png')" }}
+      >
+        <MainIcon className="mb-10 shrink-0" />
+        <h2 className="whitespace-nowrap">사장님, 감으로 장사하지 마세요. 데이터로 성공하세요!</h2>
+        <p className="text-grey-dark typo-p2-medium mt-2.5 mb-6 whitespace-nowrap">
+          AI가 내 가게 상권 분석부터 당장 실행해야 할 마케팅 전략까지 알려드립니다.
+        </p>
+        <Button onClick={() => navigate('/auth')}>내 가게 무료 진단하기</Button>
+      </section>
+
+      <section className="flex flex-col items-center w-full min-w-fit mt-50 shrink-0">
+        <SearchIcon className="shrink-0" />
+        <h2 className="mt-6 whitespace-nowrap">혹시 이런 고민 있으신가요?</h2>
+        <p className="text-grey-dark typo-p2-medium whitespace-nowrap">
+          BizScan이 데이터로 명쾌한 해답을 드립니다.
+        </p>
+
+        <div className="flex gap-[14px] mt-19 mb-50 shrink-0">
+          <DescriptionBox
+            icon={<GraphIcon className="shadow" />}
+            text1="매출이 왜 자꾸 떨어질까?"
+            text2="데이터 기반 원인 분석"
+          />
+          <DescriptionBox
+            icon={<MarketingIcon className="shadow" />}
+            text1="마케팅, 도대체 뭐부터 해야하지?"
+            text2="AI가 짜주는 맞춤형 실행전략"
+          />
+          <DescriptionBox
+            icon={<ConsultingIcon className="shadow" />}
+            text1="전문 컨설팅은 너무 비싼데.."
+            text2="비용 걱정 없는 AI 상권 코치"
+          />
         </div>
-        {/* 드롭다운 */}
-        <Select>
-          <SelectTrigger>
-            <SelectValue placeholder="Select an option" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="1">Option 1</SelectItem>
-            <SelectItem value="2">Option 2</SelectItem>
-            <SelectItem value="3">Option 3</SelectItem>
-          </SelectContent>
-        </Select>
-
-        {/* 토글 그룹 */}
-
-        <ToggleGroup type="single" defaultValue="one">
-          <ToggleGroupItem value="one">Option 1</ToggleGroupItem>
-          <ToggleGroupItem value="two">Option 2</ToggleGroupItem>
-          <ToggleGroupItem value="three">Option 3</ToggleGroupItem>
-          <ToggleGroupItem value="four">Option 4</ToggleGroupItem>
-        </ToggleGroup>
-      </div>
+      </section>
     </div>
   );
 }
