792870d9b08990f728df96de78d3a401e6b7a23c
dohyung <carl2001@naver.com>
2026-02-03 04:03:11 +0900

    [Feat] : auth api fetcher 추가

--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -1,21 +1,40 @@
-export interface LoginRequest {
-  email: string;
-  password: string;
-}
+import type { CommonResponse } from './api.type';
 
-export interface LoginResponse {
-  accessToken: string;
-  user: User;
-}
-
-export interface SignupRequest {
-  email: string;
-  password: string;
-  name: string;
-}
-
-export interface User {
+export type User = {
   id: number;
   email: string;
   name: string;
-}
+};
+
+/* REQUEST */
+export type SignupRequest = {
+  email: string;
+  nickname: string;
+  password: string;
+};
+
+export type LoginRequest = {
+  email: string;
+  password: string;
+};
+
+export type UpdateMeRequest = {
+  nickname?: string;
+};
+
+/* RESPONSE */
+export type SignupResponse = CommonResponse<User>;
+
+export type LoginResponse = CommonResponse<{
+  accessToken: string;
+}>;
+
+export type LogoutResponse = CommonResponse<string>;
+
+export type RefreshTokenResponse = CommonResponse<{
+  accessToken: string;
+}>;
+
+export type GetMeResponse = CommonResponse<User>;
+
+export type UpdateMeResponse = CommonResponse<string>;
